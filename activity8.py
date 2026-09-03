69403168173596cb72ef6643fc45876e1d48cf8e
viveromarianne7-cmd <viveromarianne7@gmail.com>
2026-09-03 12:27:00 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,22 @@
+#concatenation 
+
+food = "" #blank string
+
+f = input("What are your favorite foods --> ")
+food += f + ", "
+
+f = input("What else --> ")
+food += f + ", "
+
+
+f = input("What else --> ")
+food += f  + ", "
+
+f = input("What else --> ")
+food += f  + ", "
+
+f = input("What else --> ")
+food += f  + ", "
+
+
+print("So, my favorite foods are ", food)
