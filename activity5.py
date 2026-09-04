244fe682053a6a84d1508f669f9466ba219703d5
viveromarianne7-cmd <viveromarianne7@gmail.com>
2026-09-04 08:45:40 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,14 @@
+a = 97
+b = 12
+
+sum = a + b 
+diff = a - b 
+
+
+print ("The sum of a and b is ", sum)
+print ("The diff of a and b is ",diff)
+print ("The quotient of a and b is ",a/b)
+print ("The product of a and b is ",a*b)
+print ("The round down of a and b is ",a//b)
+print ("The exponents of a and b is ",a**b)
+print ("The percentage of a and b is ",a%b)
