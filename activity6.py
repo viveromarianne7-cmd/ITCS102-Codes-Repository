64e495750adf886e5b011c1f6a5bf7784a1cf03b
viveromarianne7-cmd <viveromarianne7@gmail.com>
2026-09-04 08:51:21 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,12 @@
+name = input(" what is your name?-")
+
+print ( " Hi my name is", name,"how are you doing today?")
+
+
+x = eval (input("Enter a number--->?"))
+
+total = x + 5						
+
+#print (" good morning,",total)
+
+print(type(x))
