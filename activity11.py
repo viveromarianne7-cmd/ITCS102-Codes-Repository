b3261c30e97ed7356273421ed11bc048b94e293e
viveromarianne7-cmd <viveromarianne7@gmail.com>
2026-09-10 11:44:45 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,10 @@
+username = "yanyan"
+password = "yan050612"
+
+user_name = input("Please enter your username: ")
+pass_word = input("Please enter your password: ")
+
+if user_name == username and pass_word == password:
+	print("Access Granted")
+else:
+	print("Access Denied")
